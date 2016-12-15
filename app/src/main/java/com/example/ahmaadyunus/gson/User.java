d2f95d3262572b5fff01ee16dad9bd4ec839e322
ahmaadyunus <scootyunus@gmail.com>
2016-12-15 09:47:21 +0700

    First Commit

---
+++ app/src/main/java/com/example/ahmaadyunus/gson/User.java
@@ -0,0 +1,28 @@
+package com.example.ahmaadyunus.gson;
+
+/**
+ * Created by ahmaadyunus on 14/12/16.
+ */
+
+public class User {
+    private int id;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+}
